6efdaba42d976572e28f10d514d03f7f373239dc
parkermarcoullier <parker@initialcapacity.io>
2026-05-27 14:02:27 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello(name: str = "World"):
+    #Return a friendly HTTP greeting
+    return {"message": f"Hello {name}!"}
